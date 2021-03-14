e72f8db67c82ae7ef5cb9a31179fa6aac74e6fb5
Jean Lima Rodovalho <jeanrodovalho@hotmail.com>
2021-03-14 07:40:34 -0300

    primeiro commit

---
+++ lib/sum_list.ex
@@ -0,0 +1,14 @@
+defmodule SumList do
+  def call(list), do: sum(list, 0)
+
+  def call_enum(list), do: Enum.sum(list)
+
+  defp sum([], acc) do
+    acc
+  end
+
+  defp sum([head | tail], acc) do
+    acc = acc + head
+    sum(tail, acc)
+  end
+end
